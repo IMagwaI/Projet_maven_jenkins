48f8a45f80bf24d41d17dd43707c7df335da3d8c
Atef <58571715+IMagwaI@users.noreply.github.com>
2021-02-11 10:46:40 +0100

    initial

---
+++ src/devops_git/main.java
@@ -0,0 +1,13 @@
+package devops_git;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Hey there");
+		System.out.println("Hey there");
+
+		System.out.println("Hey there");
+
+	}
+
+}
